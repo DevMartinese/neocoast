4df69beb2dadc5fb5451cd9deab4862f3e5b4c49
DevMartinese <devmartinese@gmail.com>
2021-09-06 02:51:34 -0300

    add(tests: finish neocoast test

--- ObjectFromArray.js
+++ ObjectFromArray.js
@@ -1,36 +1,36 @@
 /*
  Dado un arreglo de objetos, los cuales son de la forma { date: Date, items: Array<Object> } 
  retornar un objeto donde la key esel mes/año (ejemplo: 02/2020) y el valor es un arreglo con los elementos 
- cuya fecha sea la de esemes/año ordenados de forma ascendente.
+ cuya fecha sea la de ese mes/año ordenados de forma ascendente.
 */
 const worldCupChampions = [
   {
-    date: '12/07/1930',
-    items: [{ country: 'Uruguay', champion: 'Uruguay', runnerUp: 'Argentina' }],
+    date: "12/07/1930",
+    items: [{ country: "Uruguay", champion: "Uruguay", runnerUp: "Argentina" }],
   },
   {
-    date: '03/09/1978',
+    date: "03/09/1978",
     items: [
-      { country: 'Argentina', champion: 'Argentina', runnerUp: 'Holanda' },
+      { country: "Argentina", champion: "Argentina", runnerUp: "Holanda" },
     ],
   },
   {
-    date: '20/04/1966',
+    date: "20/04/1966",
     items: [
-      { country: 'Inglaterra', champion: 'Inglaterra', runnerUp: 'Alemania' },
+      { country: "Inglaterra", champion: "Inglaterra", runnerUp: "Alemania" },
     ],
   },
   {
-    date: '06/12/1998',
-    items: [{ country: 'Francia', champion: 'Francia', runnerUp: 'Brasil' }],
+    date: "06/12/1998",
+    items: [{ country: "Francia", champion: "Francia", runnerUp: "Brasil" }],
   },
 ];
 
 const newObj = () => {
   worldCupChampions
     .sort((a, b) => {
-      var aa = a.date.split('/').reverse(),
-        bb = b.date.split('/').reverse();
+      var aa = a.date.split("/").reverse(),
+        bb = b.date.split("/").reverse();
       return bb < aa ? -1 : bb > aa ? 1 : 0;
     })
     .reduce(
@@ -40,7 +40,6 @@ const newObj = () => {
       }),
       {}
     );
-  
   return worldCupChampions;
 };
 
