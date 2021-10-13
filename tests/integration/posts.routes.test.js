38008639bbbfc73badc848667f69c90221b0a85c
DevMartinese <devmartinese@gmail.com>
2021-10-13 18:43:47 -0300

    fix(): add new suit

--- tests/integration/posts.routes.test.js
+++ tests/integration/posts.routes.test.js
@@ -2,81 +2,83 @@ const request = require("supertest");
 const app = require("../../index");
 let postId;
 
-describe("Test different endpoints", () => {
-  it("POST /api/posts/create", async () => {
-    const data = {
-      title: "Hola Mundo",
-      body: "Como estan?",
-    };
-    const response = await request(app)
-      .post("/api/posts/create")
-      .send(data)
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(201);
+describe("ROUTES TEST", () => {
+  describe("Test different endpoints", () => {
+    it("POST /api/posts/create", async () => {
+      const data = {
+        title: "Hola Mundo",
+        body: "Como estan?",
+      };
+      const response = await request(app)
+        .post("/api/posts/create")
+        .send(data)
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(201);
       postId = response.body._id;
-  });
+    });
 
-  it("respond with code 400 on bad request", async () => {
-    const data = {};
-    await request(app)
-      .post("/api/posts/create")
-      .send(data)
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(400)
-      .expect('"Post not created"')
-  });
+    it("respond with code 400 on bad request", async () => {
+      const data = {};
+      await request(app)
+        .post("/api/posts/create")
+        .send(data)
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(400)
+        .expect('"Post not created"');
+    });
 
-  it("GET /api/posts/list", async () => {
-    await request(app)
-      .get("/api/posts/list")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200);
-  });
+    it("GET /api/posts/list", async () => {
+      await request(app)
+        .get("/api/posts/list")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
 
-  it("GET /api/posts/get_post/:id", async () => {
-    await request(app)
-      .get(`/api/posts/get_post/${postId}`)
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-  });
+    it("GET /api/posts/get_post/:id", async () => {
+      await request(app)
+        .get(`/api/posts/get_post/${postId}`)
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
 
-  it("respond with code 404", async () => {
-    await request(app)
-      .get("/api/posts/get_post/6134685d7f2c70efa89addeasdsd")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(404)
-  });
+    it("respond with code 404", async () => {
+      await request(app)
+        .get("/api/posts/get_post/6134685d7f2c70efa89addeasdsd")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(404);
+    });
 
-  it("PUT /api/update/post/:id", async () => {
-    const testData = {title: "Hola", body: "Neocoast"};
-    await request(app)
-      .put(`/api/update/post/${postId}`)
-      .send(testData)
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-  });
+    it("PUT /api/update/post/:id", async () => {
+      const testData = { title: "Hola", body: "Neocoast" };
+      await request(app)
+        .put(`/api/update/post/${postId}`)
+        .send(testData)
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
 
-  it("DELETE /api/delete/post/:id", async () => {
-    await request(app)
-      .delete(`/api/delete/post/${postId}`)
-      .send()
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-  });
+    it("DELETE /api/delete/post/:id", async () => {
+      await request(app)
+        .delete(`/api/delete/post/${postId}`)
+        .send()
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
 
-  it("respond with code 404", async () => {
-    await request(app)
-      .delete( '/api/delete/post/6134685d7f2c70efa89addeasdsd')
-      .send()
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(404)
-  })
+    it("respond with code 404", async () => {
+      await request(app)
+        .delete("/api/delete/post/6134685d7f2c70efa89addeasdsd")
+        .send()
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(404);
+    });
+  });
 });
--- tests/unit/posts.controller.test.js
+++ tests/unit/posts.controller.test.js
@@ -19,148 +19,150 @@ beforeEach(() => {
 
 jest.mock("../../models/Posts");
 
-describe("postsController createPost", () => {
-  beforeEach(() => {
-    req.body = newPost;
+describe("CONTROLLER FILE TEST", () => {
+  describe("postsController createPost", () => {
+    beforeEach(() => {
+      req.body = newPost;
+    });
+
+    it("should have a createPost method", () => {
+      expect(typeof createPost).toBe("function");
+    });
+
+    it("should call a Post.create", async () => {
+      await createPost(req, res);
+      expect(Posts.create).toBeCalledWith(newPost);
+    });
+
+    it("should return 201 response code", async () => {
+      await createPost(req, res);
+      expect(res.statusCode).toBe(201);
+      expect(res._isEndCalled()).toBeTruthy();
+    });
+
+    it("should return json body in response", async () => {
+      Posts.create.mockReturnValue(newPost);
+      await createPost(req, res);
+      expect(res._getJSONData()).toStrictEqual(newPost);
+    });
+
+    it("should return 400 response code", async () => {
+      req.body = {};
+      await createPost(req, res);
+      expect(res.statusCode).toBe(400);
+    });
   });
 
-  it("should have a createPost method", () => {
-    expect(typeof createPost).toBe("function");
+  describe("postsController getPosts", () => {
+    it("should have a getPosts method", () => {
+      expect(typeof getPosts).toBe("function");
+    });
+
+    it("should call a Post.find", async () => {
+      await getPosts(req, res);
+      expect(Posts.find).toHaveBeenCalledWith({});
+    });
+
+    it("should return response with status 200 and all todos", async () => {
+      Posts.find.mockReturnValue(allPosts);
+      await getPosts(req, res);
+      expect(res.statusCode).toBe(200);
+      expect(res._isEndCalled()).toBeTruthy();
+      expect(res._getJSONData()).toStrictEqual(allPosts);
+    });
+
+    it("should return response with status 404 when not found all posts", async () => {
+      Posts.find.mockReturnValue(null);
+      await getPosts(req, res);
+      expect(res.statusCode).toBe(404);
+      expect(res._isEndCalled()).toBeTruthy();
+    });
   });
 
-  it("should call a Post.create", async () => {
-    await createPost(req, res);
-    expect(Posts.create).toBeCalledWith(newPost);
+  describe("postsController getPost", () => {
+    it("should have a getPost method", () => {
+      expect(typeof getPost).toBe("function");
+    });
+
+    it("should call Posts.findById", async () => {
+      req.params.id = postId;
+      await getPost(req, res);
+      expect(Posts.findById).toBeCalledWith({ _id: postId });
+    });
+
+    it("should return response with status 200 ", async () => {
+      Posts.findById.mockReturnValue(newPost);
+      await getPost(req, res);
+      expect(res.statusCode).toBe(200);
+      expect(res._getJSONData()).toStrictEqual(newPost);
+      expect(res._isEndCalled()).toBeTruthy();
+    });
+
+    it("should return 404 when item doesn't exists", async () => {
+      Posts.findById.mockReturnValue(null);
+      await getPost(req, res);
+      expect(res.statusCode).toBe(404);
+      expect(res._isEndCalled()).toBeTruthy();
+    });
   });
 
-  it("should return 201 response code", async () => {
-    await createPost(req, res);
-    expect(res.statusCode).toBe(201);
-    expect(res._isEndCalled()).toBeTruthy();
+  describe("postsController putPost", () => {
+    it("should have a putPost method", () => {
+      expect(typeof putPost).toBe("function");
+    });
+
+    it("should update to findByIdAndUpdate", async () => {
+      req.params.id = postId;
+      req.body = newPost;
+      await putPost(req, res);
+      expect(Posts.findByIdAndUpdate).toHaveBeenCalledWith(
+        { _id: postId },
+        { $set: newPost }
+      );
+    });
+
+    it("should return http code 200 and json data", async () => {
+      req.params.id = postId;
+      req.body = newPost;
+      Posts.findByIdAndUpdate.mockReturnValue(newPost);
+      await putPost(req, res);
+      expect(res._isEndCalled).toBeTruthy();
+      expect(res.statusCode).toBe(200);
+      expect(res._getJSONData()).toStrictEqual(newPost);
+    });
+
+    it("should return 404 when item doesn't exists", async () => {
+      Posts.findByIdAndUpdate.mockReturnValue(null);
+      await putPost(req, res);
+      expect(res.statusCode).toBe(404);
+      expect(res._isEndCalled()).toBeTruthy();
+    });
   });
 
-  it("should return json body in response", async () => {
-    Posts.create.mockReturnValue(newPost);
-    await createPost(req, res);
-    expect(res._getJSONData()).toStrictEqual(newPost);
-  });
+  describe("postsController delPost", () => {
+    it("should have a delPost method", () => {
+      expect(typeof delPost).toBe("function");
+    });
 
-  it("should return 400 response code", async () => {
-    req.body = {};
-    await createPost(req, res);
-    expect(res.statusCode).toBe(400);
-  });
-});
+    it("should call findOneAndRemove", async () => {
+      req.params.id = postId;
+      await delPost(req, res);
+      expect(Posts.findOneAndRemove).toBeCalledWith({ _id: postId });
+    });
 
-describe("postsController getPosts", () => {
-  it("should have a getPosts method", () => {
-    expect(typeof getPosts).toBe("function");
-  });
+    it("should return 200 http code and deleted postsmodel", async () => {
+      Posts.findOneAndRemove.mockReturnValue(newPost);
+      await delPost(req, res);
+      expect(res.statusCode).toBe(200);
+      expect(res._getJSONData()).toStrictEqual({ msg: "Post Deleted" });
+      expect(res._isEndCalled()).toBeTruthy();
+    });
 
-  it("should call a Post.find", async () => {
-    await getPosts(req, res);
-    expect(Posts.find).toHaveBeenCalledWith({});
-  });
-
-  it("should return response with status 200 and all todos", async () => {
-    Posts.find.mockReturnValue(allPosts);
-    await getPosts(req, res);
-    expect(res.statusCode).toBe(200);
-    expect(res._isEndCalled()).toBeTruthy();
-    expect(res._getJSONData()).toStrictEqual(allPosts);
-  });
-
-  it("should return response with status 404 when not found all posts", async () => {
-    Posts.find.mockReturnValue(null);
-    await getPosts(req, res);
-    expect(res.statusCode).toBe(404);
-    expect(res._isEndCalled()).toBeTruthy();
-  })
-});
-
-describe("postsController getPost", () => {
-  it("should have a getPost method", () => {
-    expect(typeof getPost).toBe("function");
-  });
-
-  it("should call Posts.findById", async () => {
-    req.params.id = postId;
-    await getPost(req, res);
-    expect(Posts.findById).toBeCalledWith({ _id: postId });
-  });
-
-  it("should return response with status 200 ", async () => {
-    Posts.findById.mockReturnValue(newPost);
-    await getPost(req, res);
-    expect(res.statusCode).toBe(200);
-    expect(res._getJSONData()).toStrictEqual(newPost);
-    expect(res._isEndCalled()).toBeTruthy();
-  });
-
-  it("should return 404 when item doesn't exists", async () => {
-    Posts.findById.mockReturnValue(null);
-    await getPost(req, res);
-    expect(res.statusCode).toBe(404);
-    expect(res._isEndCalled()).toBeTruthy();
-  });
-});
-
-describe("postsController putPost", () => {
-  it("should have a putPost method", () => {
-    expect(typeof putPost).toBe("function");
-  });
-
-  it("should update to findByIdAndUpdate", async () => {
-    req.params.id = postId;
-    req.body = newPost;
-    await putPost(req, res);
-    expect(Posts.findByIdAndUpdate).toHaveBeenCalledWith(
-      { _id: postId },
-      { $set: newPost }
-    );
-  });
-
-  it("should return http code 200 and json data", async () => {
-    req.params.id = postId;
-    req.body = newPost;
-    Posts.findByIdAndUpdate.mockReturnValue(newPost);
-    await putPost(req, res);
-    expect(res._isEndCalled).toBeTruthy();
-    expect(res.statusCode).toBe(200);
-    expect(res._getJSONData()).toStrictEqual(newPost);
-  });
-
-  it("should return 404 when item doesn't exists", async () => {
-    Posts.findByIdAndUpdate.mockReturnValue(null);
-    await putPost(req, res);
-    expect(res.statusCode).toBe(404);
-    expect(res._isEndCalled()).toBeTruthy();
-  });
-});
-
-describe("postsController delPost", () => {
-  it("should have a delPost method", () => {
-    expect(typeof delPost).toBe("function");
-  });
-
-  it("should call findOneAndRemove", async () => {
-    req.params.id = postId;
-    await delPost(req, res);
-    expect(Posts.findOneAndRemove).toBeCalledWith({ _id: postId });
-  });
-
-  it("should return 200 http code and deleted postsmodel", async () => {
-    Posts.findOneAndRemove.mockReturnValue(newPost);
-    await delPost(req, res);
-    expect(res.statusCode).toBe(200);
-    expect(res._getJSONData()).toStrictEqual({msg: "Post Deleted"});
-    expect(res._isEndCalled()).toBeTruthy();
-  });
-
-  it('should return 404 if cant delete post', async () => {
-    Posts.findOneAndRemove.mockReturnValue(null);
-    await delPost(req, res);
-    expect(res.statusCode).toBe(404);
-    expect(res._isEndCalled()).toBeTruthy();
+    it("should return 404 if cant delete post", async () => {
+      Posts.findOneAndRemove.mockReturnValue(null);
+      await delPost(req, res);
+      expect(res.statusCode).toBe(404);
+      expect(res._isEndCalled()).toBeTruthy();
+    });
   });
 });
